3ffaa32bd240dbcd89323c6c0a73ec687c73768f
Hoang Tran <descience.thh10@gmail.com>
2026-06-30 13:40:28 +0000

    update trace gen script with 14b ckpt

--- slurm/traces_gen.sh
+++ slurm/traces_gen.sh
@@ -17,4 +17,5 @@ cd open-r1-main
 echo "Start at: $(date)"
 echo "Running on node: $(hostname)"
 
-python src/open_r1/grpo.py --config recipes/DeepSeek-R1-Distill-Qwen-7B/grpo/config_demo.yaml --model_name_or_path /mnt/data/vhoangth2/pretrained-weights/DeepSeek-R1-Distill-Qwen-7B --dataset_name open-r1/OpenR1-Math-220k --dataset_prompt_column problem --save_dir traces/ --num_generations 2 --beta 0 --report_to none --use_vllm False --max_completion_length 8192 --do_train False --trace_only --trace_tokens 1_000_000 
+# python src/open_r1/grpo.py --config recipes/DeepSeek-R1-Distill-Qwen-7B/grpo/config_demo.yaml --model_name_or_path /mnt/data/vhoangth2/pretrained-weights/DeepSeek-R1-Distill-Qwen-7B --dataset_name open-r1/OpenR1-Math-220k --dataset_prompt_column problem --save_dir traces/ --num_generations 2 --beta 0 --report_to none --use_vllm False --max_completion_length 8192 --do_train False --trace_only --trace_tokens 1_000_000 
+python src/open_r1/grpo.py --config recipes/DeepSeek-R1-Distill-Qwen-14B/grpo/config_demo.yaml --model_name_or_path /mnt/data/vhoangth2/pretrained-weights/DeepSeek-R1-Distill-Qwen-14B --dataset_name open-r1/OpenR1-Math-220k --dataset_prompt_column problem --save_dir traces/ --num_generations 2 --beta 0 --report_to none --use_vllm False --max_completion_length 8192 --do_train False --trace_only --trace_tokens 1_000_000 
